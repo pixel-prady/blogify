368b0392a692f5155f9fc22c15db2cb2d56f466e
Pradhuman Chaudhary <121712518+pixel-prady@users.noreply.github.com>
2025-07-16 01:13:29 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,33 +3,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 
-// app.use(
-//     cors({
-//         origin: process.env.CORS_ORIGIN,
-//         credentials: true,
-//     })
-// );
-const allowedOrigins = [
-  "https://blogify-phi-brown.vercel.app",
-  "http://localhost:3000" // for dev
-];
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); 
-  }
-
-  next();
-});
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 
 app.use(express.json({ limit: "100kb" }));
 app.use(express.urlencoded({ extended: true, limit: "100kb" }));
